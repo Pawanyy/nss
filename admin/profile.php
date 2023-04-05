01b96062e9b4f086d6ebc9e46bb9e2100a2c4ea1
Pawanyy <pawanyyy01@gmail.com>
2023-04-06 02:54:25 +0530

    admin profile and password change

--- admin/profile.php
+++ admin/profile.php
@@ -3,11 +3,16 @@
 $helper->IsAccessibleByAdmin();
 
 $user_id = $_SESSION['uid'];
+$sql = "SELECT * FROM tbl_users WHERE id = $user_id";
+$result = $conn -> query($sql);
+$user_row = $result -> fetch_assoc();
+
 if(isset($_POST['passwordUpdate'])){
     $old_password = $_POST['oldPassword'];
     $password = $_POST['newPassword'];
+    $role = ROLE::ADMIN;
     
-    $sql = "SELECT * FROM `tbl_users` WHERE id = $user_id AND password = '$old_password'";
+    $sql = "SELECT * FROM `tbl_users` WHERE id = $user_id AND password = '$old_password' AND role = $role";
     $result = $conn -> query($sql);
     $row = $result -> fetch_assoc();
 
@@ -18,73 +23,77 @@ if(isset($_POST['passwordUpdate'])){
 
         if($resultUpdate){
             $helper->SendSuccessToast("Password Changed Sucessfully");
-            $helper->Redirect(BASE_URL . 'profile.php');
+            $helper->Redirect(ADMIN_URL . 'profile.php');
         } else {
             $helper->SendErrorToast("Password Update Failed!!");
-            $helper->Redirect(BASE_URL . 'profile.php');
+            $helper->Redirect(ADMIN_URL . 'profile.php');
         }
     } else {
         $helper->SendErrorToast("Old Password Incorrect!!");
-        $helper->Redirect(BASE_URL . 'profile.php');
+        $helper->Redirect(ADMIN_URL . 'profile.php');
     }
 }
-
-$sql = "SELECT * FROM tbl_contact";
-$result = $conn -> query($sql);
-$rows = $result -> fetch_all(MYSQLI_ASSOC);
 ?>
 <?php 
-$title = "Contact";
+$title = "Profile";
 require_once __DIR__ . "/include/layout-start.php"; 
 ?>
-<div class="py-5">
-<div class="table-responsive">
-    <table class="table table-bordered  table-striped">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Subject</th>
-                <th scope="col">Message</th>
-                <th scope="col">DateTime</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-            $sl = 0;
-            foreach ($rows as $key => $value) { 
-                $sl++;
-            ?>
-                <tr>
-                    <th scope="row"><?=$sl?></th>
-                    <td><?=$value['name']?></td>
-                    <td><?=$value['email']?></td>
-                    <td><?=$value['phone']?></td>
-                    <td><?=$value['subject']?></td>
-                    <td><?=$value['message']?></td>
-                    <td><?=$value['date']?></td>
-                    <td>
-                        <a href="<?=ADMIN_URL?>contact.php?did=<?=$value['id']?>"
-                            onclick="return confirm('Are you sure?')">
-                            Delete
-                        </a>
-                    </td>
-                </tr>
-            <?php } ?>
-            <?php if($sl == 0) { ?>
-                <tr>
-                    <td colspan="10">
-                        <div class="text-center py-2">
-                            No Users
+<div class="p-0">
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <div class="card border border-1">
+                <div class="card-header">
+                    <h4 class="card-title m-0">Profile</h4>
+                </div>
+                <div class="card-body">
+                    <form method="post">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" value="<?=$user_row['name']?>" required disabled>
                         </div>
-                    </td>
-                </tr>
-            <?php } ?> 
-        </tbody>
-    </table>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" value="<?=$user_row['email']?>" required disabled>
+                        </div>
+                        <div class="mb-3">
+                            <label for="phone" class="form-label">Phone</label>
+                            <input type="tel" class="form-control" id="phone" name="phone" value="<?=$user_row['phone']?>" required disabled>
+                        </div>
+                        <div class="mb-3">
+                            <label for="aboutme" class="form-label">Amout Me</label>
+                            <textarea class="form-control" id="aboutme" name="aboutme" required disabled><?=$user_row['aboutme']?></textarea>
+                        </div>
+                        <!-- <div class="mb-3">
+                            <label for="reg" class="form-label">Registration Date</label>
+                            <input type="text" class="form-control" id="reg" name="reg" value="<?=$user_row['created_at']?>" disabled>
+                        </div>
+                        <button type="submit" name="profileUpdate" class="btn btn-primary">Update</button> -->
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card border border-1">
+                <div class="card-header">
+                    <h4 class="card-title m-0">Change Password</h4>
+                </div>
+                <div class="card-body">
+                    <form method="post">
+                        <div class="mb-3">
+                            <label for="oldPassword" class="form-label">Old Password</label>
+                            <input type="password" class="form-control" id="oldPassword" name="oldPassword" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="newPassword" class="form-label">New Password</label>
+                            <input type="password" class="form-control" id="newPassword" name="newPassword" required>
+                        </div>
+                        <button type="submit" name="passwordUpdate" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 <?php require_once __DIR__ . "/include/layout-end.php"; ?>
